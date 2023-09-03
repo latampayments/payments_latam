b5ad1631b435e288e5bac9ee8d9214cb8f2efa18
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-03 02:06:51 -0300

    :books: build home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,66 @@
 import { buttonVariants } from '@/components/ui/button'
+import prisma from '@/lib/db';
 import Link from 'next/link'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { GetServerSideProps, NextPage } from "next";
+import Image from 'next/image';
 
-export default function Home() {
+interface Country {
+  id: string;
+  country: string;
+  flag: string;
+}
+
+interface CountryProps {
+  countries: Country[];
+}
+
+export default function Home({countries}: CountryProps) {
+  console.log(countries)
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Home</h1>
-      <Link className={buttonVariants()} href="/dashboard">Dashboard</Link>
+    <div className="mt-2 justify-center p-2">
+      <div className="w-full flex flex-wrap right-0 justify-around space-x-2 space-y-2 items-center lg:justify-between sm:justify-center md:justify-center">
+        {countries.map((ct) => 
+        <Link key={ct.id} href={`/country/${ct.id}`}>
+          <Card>
+            <CardHeader>
+              <CardTitle>ct.country</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Image
+                  className="rounded-sm h-48 w-48 object-cover object-center"
+                  src={ct.flag}
+                  alt={ct.country}
+                />
+            </CardContent>
+          </Card>
+        </Link>
+        )}
+      </div>
     </div>
   )
 }
 
-export const getStaticProps = async () => {
+export const getServerSideProps: GetServerSideProps<CountryProps> = async() => {
+  const users = await prisma.user.findMany()
+  console.log(users)
+  const countries: Country[] = await prisma.country.findMany({
+    select: {
+      id: true,
+      country: true,
+      flag: true,
+    }
+  });
   return {
-    props: {},
-  }
+    props: {
+      countries,
+    },
+  };
 }
