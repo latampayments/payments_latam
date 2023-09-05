8180c0c23496aa7bab60d445304c1f61462f6c73
orafaelvinicius <contato.rafaelvinicius@gmail.com>
2023-09-05 20:24:49 -0300

    [feat] page cart init

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,25 @@
-'use Client'
-import React, { useState, useEffect } from "react";
+"use Client";
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import prisma from "@/lib/db";
-import { Layout, Table } from "lucide-react";
+import Link from "next/link";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 interface CartItem {
   id: number;
@@ -12,16 +29,18 @@ interface CartItem {
 }
 
 const CartPage = () => {
-  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+  //   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-  const itens:CartItem[] = [{
-    id: 1,
-    productName: 'Monthly package',
-    quantity: 1,
-    unitPrice: 100,
-  }]
+  const cartItems: CartItem[] = [
+    {
+      id: 1,
+      productName: "Monthly package",
+      quantity: 1,
+      unitPrice: 100,
+    },
+  ];
+  // setCartItems(itens)
 
-  setCartItems(itens)
   //   useEffect(() => {
   //     // Faça uma chamada à sua API ou banco de dados para buscar os itens do carrinho do usuário
   //     async function fetchCartItems() {
@@ -51,37 +70,54 @@ const CartPage = () => {
   //   };
 
   return (
-    <Layout>
-      <h1>Seu Carrinho</h1>
-      <Table>
-        <thead>
-          <tr>
-            <th>Produto</th>
-            <th>Quantidade</th>
-            <th>Preço Unitário</th>
-            <th>Preço Total</th>
-            <th>Ação</th>
-          </tr>
-        </thead>
-        <tbody>
-          {cartItems.map((item) => (
-            <tr key={item.id}>
-              <td>{item.productName}</td>
-              <td>{item.quantity}</td>
-              <td>{item.unitPrice}</td>
-              <td>{item.quantity * item.unitPrice}</td>
-              <td>
-                <Button onClick={() => removeItemFromCart(item.id)}>
-                  Remover
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      {/* Adicione o resumo do pedido e opções de pagamento aqui */}
-      <Button onClick={() => router.push("/checkout")}>Finalizar Compra</Button>
-    </Layout>
+    <>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Your cart</CardTitle>
+            <CardDescription>Itens in your cart</CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <Table>
+          
+              {/* <TableCaption>A list of your recent invoices.</TableCaption> */}
+
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[100px]">Produto</TableHead>
+                  <TableHead>Quantidade</TableHead>
+                  <TableHead className="text-right">Preço Unitário</TableHead>
+                  <TableHead className="text-right">Preço Total</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {cartItems.map((item) => (
+                <TableRow key={item.id}>
+           
+                  <TableCell className="font-medium">{item.productName}</TableCell>
+                  <TableCell>{item.quantity}</TableCell>
+                  <TableCell className="text-right">${item.unitPrice}</TableCell>
+                  <TableCell className="text-right">${item.quantity * item.unitPrice}</TableCell>
+                </TableRow>
+              ))}
+                
+              </TableBody>
+            </Table>
+          </CardContent>
+
+          <CardFooter>
+            <div className="w-full flex flex-wrap right-0 justify-around space-x-4 space-y-4 items-center lg:justify-between sm:justify-center md:justify-center">
+              <Button>Back to home</Button>
+
+              <Link href={`/method/`} className="justify-center text-center" />
+
+              <Button>Checkout</Button>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
   );
 };
 
