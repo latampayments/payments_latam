e5e690a7ac0ac4d49ab92e703f0320429e763746
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-05 22:44:50 -0300

    :books: adding seed

--- prisma/seed.js
+++ prisma/seed.js
@@ -86,7 +86,8 @@ async function seed() {
       country: "Brazil",
       flag: "https://flagicons.lipis.dev/flags/4x3/br.svg",
       banks: {
-        create: [{
+        create: [
+        {
           name: "Santander",
           logo: "https://thumbs2.imgbox.com/ef/aa/Kv6AZKQK_t.png",
           payment: {
@@ -103,7 +104,7 @@ async function seed() {
                   st2_text: "Select Pix option and click on the Código QR to scan the code (if you have the client on the phone, remember that the client need to handle the camera to scan the code from your plataform), finish to fulfill the transfer anc click on continue util the code input to confirm the transfer."
                 }]
               },
-            },
+            },            
             {
               type: "debit",
               symbol: "https://thumbs2.imgbox.com/b0/24/MfVjoT8P_t.png",
@@ -133,7 +134,71 @@ async function seed() {
               },
             }]
           }
-        }, {
+        },
+        {
+          name: "Caixa Econômica",
+          logo: "https://thumbs2.imgbox.com/29/f4/LXXzquqv_t.png",
+          payment: {
+            create: [
+              {
+              type: "transfer",
+              symbol: "https://logospng.org/download/pix/logo-pix-1024.png",
+              limits: "BRL 10,000/day (individuals) and BRL 50,000/day (legal entities);",
+              information: "Work with high amount if the client register the beneficiary. Have a block after 8:00h PM (Brazilian timezone) for high amount. To register the beneficiary for high amount and also transfer follow these steps.",
+              steps: {
+                create: [
+                  {
+                  st1_pic: "https://thumbs2.imgbox.com/0d/a3/ZvzMiIrp_t.png",
+                  st1_text: "The client can access the CEF online banking, click on the option to CEF Online Banking.",
+                  st2_pic: "https://thumbs2.imgbox.com/14/2d/FN7ajY3E_t.png",
+                  st2_text: "The client have in the app the symbol of pix and must click to open the options to pay with Pix and to register the beneficiary for high amount.",
+                  st3_pic: "https://thumbs2.imgbox.com/86/ff/hy2zb7xP_t.png",
+                  st3_text: "To access the beneficiary, the client need first do a payment with the PIX code. So after the client selected the Payment option need to add the PIX code or scan the QR code. Will be 2 simple steps, confirm the key, input the amount and click on continue.",
+                  st4_pic: "https://thumbs2.imgbox.com/e6/44/maRLRmc6_t.png",
+                  st4_text: "After step to complete the transaction is to insert the pin with 6 numbers. Consider that for high amount and for new beneficiary the client will need to scan his face and maybe other confirmations.",
+                  st5_pic: "https://thumbs2.imgbox.com/f4/20/5vOslQ63_t.png",
+                  st5_text: "The client will receive a confirmation of the transaction, the credit take few seconds, if you not receive the client must try again.", 
+                  st6_pic: "https://thumbs2.imgbox.com/10/b1/b72XNf9P_t.png",
+                  st6_text: "After the client receive the ok, need to scroll down and select the Favorite option.",
+                }
+              ]
+              },
+            },
+            {
+              type: "debit",
+              symbol: "https://thumbs2.imgbox.com/29/f4/LXXzquqv_t.png",
+              limits: "Card block for any kind of international transactions.",
+              information: "CEF debit card is blocked for any kind of international transactions.",
+              steps: {
+                create: [{
+                  st1_pic: "",
+                  st1_text: "",
+                  st2_pic: "",
+                  st2_text: ""
+                }]
+              }
+          },
+          {
+            type: "credit",
+            symbol: "https://thumbs2.imgbox.com/03/e9/uS3P9rSP_t.png",
+            limits: "Variable by clients history, been the minimum R$ 1.000,00. The client can consult the credit card limit on your invoice, on the CAIXA Cards Application or on the CAIXA Internet Banking.",
+            information: "CEF credit cards can be used for international transactions. To do so, you will need to activate the international transaction feature for your card. You can do this by contacting CEF customer service or by logging into your CEF online banking account and following the instructions.",
+            steps: {
+              create: [{
+                st1_pic: "https://thumbs2.imgbox.com/0d/a3/ZvzMiIrp_t.png",
+                st1_text: "The client can access the CEF online banking, click on the option to CEF Online Banking.",
+                st2_pic: "https://thumbs2.imgbox.com/85/a9/Hd6CH88N_t.png",
+                st2_text: "Go to option (right corner), after click will open the menu with option and the client must scroll down and select Credit Card (Cartão de Crédito).",
+                st3_pic: "https://thumbs2.imgbox.com/f7/90/NMw8I5N1_t.png",
+                st3_text: "In the bottom of the screen, click on the option to Allow Compra na Internet, verify the best PSP for this card.",
+                st4_pic: "https://thumbs2.imgbox.com/29/c3/ERLFhPYs_t.png",
+                st4_text: "Choose the option to allow online shopping and confirm."
+              }]
+            },
+          }]
+        },
+        }, 
+        {
           name: "Banco do Brasil",
           logo: "https://logopng.com.br/logos/banco-do-brasil-5.png",
           payment: {
