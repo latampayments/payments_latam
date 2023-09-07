23ef31fa5fe4b09ef89c02093db28879cd0c688b
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-07 00:09:34 -0300

    :bug: bug on deploy

--- src/app/method/[methodId]/page.tsx
+++ src/app/method/[methodId]/page.tsx
@@ -5,7 +5,6 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
-
 type Params = {
   params: {
     methodId: string
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,20 +8,22 @@ import UserLogout from './ui/UserLogout';
 const Navbar = async() => {
   const session = await getServerSession(authOptions);
   return (
-    <div className=' bg-[#F6F6F6] py-2 border-b border-s-zinc-200 w-full'>
-      <div className='flex items-center justify-around my-4'>
-        <Link href='/'>
-        <Home />
-        </Link>
-        {session ? (
-          <UserLogout />
-        ) :
-        <Link className={buttonVariants()} href='/sign-in'>
-          Sign in
-        </Link>
-        }
+    <>
+      <div className='bg-[#F6F6F6] py-2 border-b border-s-zinc-200 w-full'>
+        <div className='flex items-center justify-around my-4'>
+          <Link href='/'>
+          <Home />
+          </Link>
+          {session ? (
+            <UserLogout />
+          ) :
+          <Link className={buttonVariants()} href='/sign-in'>
+            Sign in
+          </Link>
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
