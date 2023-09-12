e1ceab1f795454e0ac8559c3b7d5c384f6c088b3
felipeLx <felipealvesfinanceiro@gmail.com>
2023-09-11 22:42:47 -0300

    :lipstick: css and layout

--- src/app/bank/[bankId]/page.tsx
+++ src/app/bank/[bankId]/page.tsx
@@ -28,30 +28,27 @@ export default async function Bank({ params: { bankId } }: Params) {
   }
 
   return (
-    <div className="flex justify-center">
-      <div className="w-full flex flex-wrap space-x-4 space-y-4 lg:justify-between sm:justify-center md:justify-center">
-        {methods.map((ct) => 
-        <Link key={ct.id} href={`/method/${ct.id}`}>
-        <Card className='bg-[#F6F6F6] justify-center items-center px-1'>
-            <CardHeader>
-            <CardTitle>{ct.type}</CardTitle>
-            <CardDescription>Limite {ct.limits}</CardDescription>
-            </CardHeader>
-            <CardContent className="flex flex-col items-center justify-center">
-              <p className=''>{ct.information}</p>
-              <Image
-                  className="rounded-sm h-48 w-48 object-cover object-center"
-                  src={ct?.symbol}
-                  alt={ct.type}
-                  width={400}
-                  height={400}
-              />
-            </CardContent>
-        </Card>
-        </Link>
-        )}
+    <div className="w-full flex flex-wrap space-x-4 justify-around items-start">
+      {methods.map((ct) => 
+      <Link key={ct.id} href={`/method/${ct.id}`}>
+      <Card className='bg-[#F6F6F6] justify-center items-center w-96 flex flex-col px-1'>
+          <CardHeader>
+          <CardTitle>{ct.type}</CardTitle>
+          <CardDescription>Limite {ct.limits}</CardDescription>
+          </CardHeader>
+          <CardContent className="justify-center">
+            <p className=''>{ct.information}</p>
+            <Image
+                className="rounded-sm h-48 w-48 object-cover object-center"
+                src={ct?.symbol}
+                alt={ct.type}
+                width={400}
+                height={400}
+            />
+          </CardContent>
+      </Card>
+      </Link>
+      )}
     </div>
-    </div>
-    
-    )
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,27 +13,25 @@ export default async function Home() {
 
   return (
     <>
-      <div className="flex py-2 px-2">
-        <div className="w-full flex flex-wrap justify-around space-x-2 space-y-2 items-center lg:justify-between sm:justify-center md:justify-center">
-          {countries.map((ct) => 
-          <Link key={ct.id} href={`/country/${ct.id}`} className='justify-center text-center'>
-            <Card className=' py-2 bg-[#F6F6F6]'>
-              <CardHeader>
-                <CardTitle>{ct.country}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <Image
-                    className="rounded-sm h-48 w-48 object-cover object-center"
-                    src={ct.flag}
-                    alt={ct.country}
-                    width={400}
-                    height={400}
-                  />
-              </CardContent>
-            </Card>
-          </Link>
-          )}
-        </div>
+      <div className="flex w-full flex-wrap justify-around space-x-4 items-start">
+        {countries.map((ct) => 
+        <Link key={ct.id} href={`/country/${ct.id}`} >
+          <Card className='flex flex-col bg-[#F6F6F6] mt-4'>
+            <CardHeader>
+              <CardTitle>{ct.country}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Image
+                  className="rounded-sm h-48 w-48 object-cover object-center"
+                  src={ct.flag}
+                  alt={ct.country}
+                  width={400}
+                  height={400}
+                />
+            </CardContent>
+          </Card>
+        </Link>
+        )}
       </div>
     </>
   )
